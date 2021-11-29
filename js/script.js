3beb578b3601e18c726aacd6019341d82a3a2cb9
XanderGale <alessandro.galeazzi@icloud.com>
2021-11-29 01:09:01 +0100

    step2

--- js/script.js
+++ js/script.js
@@ -4,14 +4,46 @@
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
-const student = 
-{
+
+// 1st Step:
+// const student = 
+// {
+//     studentName: 'Alessandro',
+//     studentSurname: 'Galeazzi',
+//     studentEta: 25
+// };
+
+
+// for (let key in student){
+//     alert( key + ' ' + student[key]);
+// }
+
+
+// Step 2
+const students =
+[
+    {
     studentName: 'Alessandro',
     studentSurname: 'Galeazzi',
     studentEta: 25
-};
+    },
+    {
+        studentName: 'Giuseppe',
+        studentSurname: 'Salentino',
+        studentEta: 19
+    },
+    {
+        studentName: 'Pietro',
+        studentSurname: 'Berardi',
+        studentEta: 21
+    },
+];
 
-
-for (let key in student){
-    alert( key + ' ' + student[key]);
+for (let i = 0; i < students.length; i++){    
+    alert(`
+    Nome: ${students[i].studentName}
+    Cognome: ${students[i].studentSurname}
+    Età: ${students[i].studentEta}
+    `)
 }
+
